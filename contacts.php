2ad4902a1b41b87248b1d813e1f2321082c7d6db
Tukhtaboy <stoprich@gmail.com>
2021-02-27 20:11:05 +0500

    Contacts Us fixed
    I have fixed background image size and Contacts us message position.

--- contacts.php
+++ contacts.php
@@ -2,7 +2,11 @@
 
 <link rel="stylesheet" href="css/contacts.css">
 
+<<<<<<< Updated upstream
 <div class="container-fluid p-0 mb-5">
+=======
+<div class="container-fluid p-0">
+>>>>>>> Stashed changes
     <div id="aboutUs">
         <div id="aboutUsBg">
             <div id="blackLayer">
@@ -10,8 +14,12 @@
                     <h1 class="text-center col-lg-12">
                         LEARN MORE ABOUT US
                     </h1>
+<<<<<<< Updated upstream
                     <a href="#" class="col-lg-12 brand-text"
                        style="z-index:1; width: fit-content; font-size: 30px; text-decoration: none;">
+=======
+                    <a href="#" class="col-lg-12 brand-text" style="z-index:1; width: fit-content; font-size: 30px; text-decoration: none;">
+>>>>>>> Stashed changes
                         <img src="images/brand-icon.png" alt="Sushi Heaven Icon" style="width: 60px;
                                 height: auto;"> Sushi Heaven
                     </a>
@@ -23,9 +31,13 @@
             <div class="container-fluid pl-5">
                 <div class="row mt-5 ">
                     <div class="col-md-6  ml-5" style="margin-left: 100px; margin-right: 100px;" id="mapFinland">
+<<<<<<< Updated upstream
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.8289051439529!2d24.950312716180647!3d60.167011250617115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46910b183d098b0d%3A0x7367e508c5613991!2sFinlandia%20Caviar%20Shop%20%26%20Restaurant!5e0!3m2!1sru!2s!4v1611154927892!5m2!1sru!2s"
                                 width="100%" height="500px" frameborder="0"
                                 style="border: 1px solid #111; border-radius: 10px;" tabindex="0"></iframe>
+=======
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.8289051439529!2d24.950312716180647!3d60.167011250617115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46910b183d098b0d%3A0x7367e508c5613991!2sFinlandia%20Caviar%20Shop%20%26%20Restaurant!5e0!3m2!1sru!2s!4v1611154927892!5m2!1sru!2s" width="100%" height="500px" frameborder="0" style="border: 1px solid #111; border-radius: 10px;" tabindex="0"></iframe>
+>>>>>>> Stashed changes
 
                     </div>
                     <div class="col-md-4 align-items-center">
@@ -59,7 +71,11 @@
                                 </div>
                                 <div class="col-md-10 content">
                                     <h2 style="font-weight: 600;">Email:</h2>
+<<<<<<< Updated upstream
                                     <p>sushiheaven2021@gmail.com</p>
+=======
+                                    <p>hello@sushiheaven.fi</p>
+>>>>>>> Stashed changes
                                 </div>
                             </div>
                         </div>
@@ -72,6 +88,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-2"></div>
+<<<<<<< Updated upstream
 <!--                <form action="" method="post">-->
 <!--                    First Name: <input type="text" name="first_name"><br>-->
 <!--                    Last Name: <input type="text" name="last_name"><br>-->
@@ -95,6 +112,22 @@
                         </div>
                         <div class="form-group text-center">
                                 <button type="button" onclick="sendEmail()" value="Send an email" class="submitButton">Submit</button>
+=======
+                <div class="col-md-8">
+                    <h1 class="text-center">Contact Us</h1>
+                    <form action="" method="post" style="margin-bottom: 15px;">
+                        <div class="form-group">
+                            <textarea name="message" id="message" cols="30" rows="5" class="form-control mb-4" placeholder="Message"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control mb-4" placeholder="Your Name">
+                        </div>
+                        <div class="form-group mb-4">
+                            <input type="email" class="form-control" placeholder="Your Email">
+                        </div>
+                        <div class="form-group text-center">
+                            <a class="submitButton" href="#" type="submit">SUBMIT</a>
+>>>>>>> Stashed changes
                         </div>
                     </form>
                 </div>
@@ -102,6 +135,7 @@
             </div>
         </div>
     </div>
+<<<<<<< Updated upstream
 </div>
 <script src="http://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script>
@@ -129,5 +163,10 @@
 
 
 
+=======
+>>>>>>> Stashed changes
 
-<?php include 'footer.php' ?>
+    <div class="col-lg-12 col-md-12 col-sm-12">
+        <?php include 'footer.php' ?>
+    </div>
+</div>
