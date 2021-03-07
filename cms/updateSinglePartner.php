046644f34c9ccbb0a3aa08af5027db066e33f939
romatsypin <62996201+romatsypin@users.noreply.github.com>
2021-03-07 13:04:30 +0200

    Create updateSinglePartner.php

---
+++ cms/updateSinglePartner.php
@@ -0,0 +1,43 @@
+<?php
+include 'db.php';
+
+$a = $_GET["id"];
+$partner = $dbHandle->getQuery("SELECT * FROM partners WHERE id='$a'");
+ ?>
+
+ <a href="adminPartners.php"><img src="../images/back.png" alt="" style="width:50px"></a>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Update Partner</title>
+   </head>
+   <body>
+     <form class="" method="post" enctype="multipart/form-data">
+     Partner Image:<br>
+     <img src="<?php echo "../".$partner["image"] ?>" alt=""> <br>
+     <input type="file" name="image" accept="image/*"> <br>
+     Partner Image Description:<br>
+     <input type="text" name="imageAlt" value="" required><br>
+     <input type="submit" name="submit" value="Save Data" >
+    </form>
+
+      <?php
+      
+      if(isset($_POST['submit']))
+      {
+        $pImage = $_FILES['image']['name'];
+        $pImageAlt = $_POST['imageAlt'];
+        $imagesFolder = "../images/".basename($pImage);
+
+        $query = $dbHandle->insertQuery("UPDATE partners set image='images/$pImage', imageAlt='$pImageAlt' WHERE id='$a'");
+
+        $imageMoveToFolder = move_uploaded_file($_FILES['image']['tmp_name'], $imagesFolder);
+
+        echo "Partner info was updated. <br> Please return back to Partner Management to see changes";
+      }
+
+       ?>
+   </body>
+ </html>
